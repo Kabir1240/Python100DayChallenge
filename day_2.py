b5542cac5c5bb4c84f09a9e2a1f052ab7f83f8e1
Kabir1240 <71541376+Kabir1240@users.noreply.github.com>
2024-03-10 17:10:50 +0500

    Add files via upload

--- day_2.py
+++ day_2.py
@@ -3,13 +3,14 @@ def tip_calculator():
     print("Welcome to the tip calculator!")
 
     # get necessary data
-    total_bill = float(input("What was the total bill?\n$"))
+    bill = float(input("What was the total bill?\n$"))
     tip_amount = int(input("How much tip would you like to give? 10, 12 or 15%?\n"))
     n_people = int(input("How many people to split the bill?\n"))
 
     # calculations
-    total_bill = round((total_bill * (1 + (tip_amount/100))) / n_people, 2)
-    print(f"Each person should pay: ${total_bill}")
+    total_bill = round((bill * (1 + (tip_amount/100))) / n_people, 2)
+    bill_as_string = "{:.2f}".format(total_bill)
+    print(f"Each person should pay: ${bill_as_string}")
 
 
 tip_calculator()
