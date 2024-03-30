6dc3d1e90b55684bd0961847efc1231df1e73943
Kabir Kashif <kashif.kabir60@gmail.com>
2024-03-30 16:42:01 +0500

    Completed day 15, coffee machine oop version

--- code/day_15/day_15.py
+++ code/day_15/day_15.py
@@ -2,3 +2,27 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+
+
+def coffee_machine_oop():
+    coffee_maker = CoffeeMaker()
+    money_machine = MoneyMachine()
+    menu = Menu()
+
+    while True:
+        user_input = input(f"What would you like? ({menu.get_items()}):\n")
+        while user_input.lower() not in ["espresso", "latte", "cappuccino", "off", "report"]:
+            user_input = input("INVALID INPUT\nPLease try again (espresso/latte/cappuccino/off):\n")
+
+        if user_input.lower() == "off":
+            print("Exiting...")
+            break
+        elif user_input.lower() == "report":
+            coffee_maker.report()
+            money_machine.report()
+        else:
+            coffee = menu.find_drink(user_input.lower())
+            if coffee_maker.is_resource_sufficient(coffee) and money_machine.make_payment(coffee.cost):
+                    coffee_maker.make_coffee(coffee)
+
+coffee_machine_oop()
