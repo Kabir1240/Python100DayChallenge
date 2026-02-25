a0e2b49e2a0dcc6c314a24406b7aaa935ffb3603
Kabir <kashif.kabir60@gmail.com>
2026-02-25 09:09:25 +0500

    updated RPS to be more readable

--- code/day_4.py
+++ code/day_4.py
@@ -34,31 +34,39 @@ def rps():
         """
 
     rps_art = [rock_art, paper_art, scissors_art]
-
-    # 0 = rock, 1 = paper, 2 = scissors
-    cpu_choice = random.randint(0, 2)
-    player_choice = int(input("What do you choose? Type 1 for rock, 2 for paper and 3 for scissors.\n")) - 1
-
-    if player_choice >= 3 or player_choice < 0:
-        print("Invalid Input, please choose type 1, 2 or 3")
-    else:
-        # Display both choices
-        print("You Chose: ")
-        print(rps_art[player_choice])
-        print("The CPU chose: ")
-        print(rps_art[cpu_choice])
-
-        # logic
-        if player_choice == 2 and cpu_choice == 0:
-            print("You Lose...")
-        elif cpu_choice == 2 and player_choice == 0:
-            print("Congrats! You Won!")
-        elif cpu_choice > player_choice:
-            print("You Lose...")
-        elif player_choice > cpu_choice:
-            print("Congrats! You Won!")
-        elif player_choice == cpu_choice:
-            print("It Was a Draw :(")
-
+    
+    wins_against = {
+        0: 2,  # Rock beats Scissors
+        1: 0,  # Paper beats Rock
+        2: 1   # Scissors beats Paper
+    }
+    
+    while True:
+        try:
+            # Pick choices for player and cpu
+            # 0 = rock, 1 = paper, 2 = scissors 3 = quit
+            player_choice = int(input("What do you choose? Type 1 for rock, 2 for paper and 3 for scissors and 4 to exit.\n")) - 1
+            
+            if player_choice == 3:
+                print("Thanks for playing!")
+                break
+            
+            cpu_choice = random.randint(0, 2)
+        
+            # Display both choices
+            print(f"You Chose: {rps_art[player_choice]}")
+            print(f"The CPU chose: {rps_art[cpu_choice]}")
+            
+            # logic
+            if player_choice == cpu_choice:
+                print("It's a draw!")
+            elif wins_against[player_choice] == cpu_choice:
+                print("You win!")
+            else:
+                print("You lose!")
+                
+        except ValueError:
+            print("Invalid Input, please choose type 1, 2 or 3")
+            continue
 
 rps()
